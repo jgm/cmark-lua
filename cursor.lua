b228bfdfd919e39083ffe3a480b92780fa591a00
John MacFarlane <jgm@berkeley.edu>
2014-12-05 23:08:17 -0800

    Improved cursor.lua.

--- cursor.lua
+++ cursor.lua
@@ -32,8 +32,14 @@ function Cursor.new()
 
    C.pointer = nil
 
-   C.type = function()
-      return type_table[cmark.cmark_node_get_type(C.pointer)]
+   C.top = function()
+      while C.parent() do end
+   end
+
+   C.__gc = function()
+      -- goto root of document, then free
+      C.top()
+      cmark.cmark_node_free(C.pointer)
    end
 
    C.next = function()
@@ -76,6 +82,84 @@ function Cursor.new()
       end
    end
 
+   C.parent = function()
+      local pointer = cmark.cmark_node_parent(C.pointer)
+      if pointer == nil then
+         return false
+      else
+         C.pointer = pointer
+         return true
+      end
+   end
+
+   C.get_type = function()
+      return cmark.cmark_node_get_type(C.pointer)
+   end
+
+   C.get_type_string = function()
+      return type_table[C.get_type()]
+   end
+
+   C.get_string_content = function()
+      return cmark.cmark_node_get_string_content(C.pointer)
+   end
+
+   C.set_string_content = function(s)
+      return cmark.cmark_node_set_string_content(C.pointer, s)
+   end
+
+   C.get_url = function()
+      return cmark.cmark_node_get_url(C.pointer)
+   end
+
+   C.set_url = function(s)
+      return cmark.cmark_node_set_url(C.pointer, s)
+   end
+
+   C.get_title = function()
+      return cmark.cmark_node_get_title(C.pointer)
+   end
+
+   C.set_title = function(s)
+      return cmark.cmark_node_set_title(C.pointer, s)
+   end
+
+   C.get_list_type = function()
+      return cmark.cmark_node_get_list_type(C.pointer)
+   end
+
+   C.set_list_type = function(list_type)
+      return cmark.cmark_node_set_list_type(C.pointer, list_type)
+   end
+
+   C.get_list_tight = function()
+      return cmark.cmark_node_get_list_tight(C.pointer)
+   end
+
+   C.set_list_tight = function(list_tight)
+      return cmark.cmark_node_set_list_tight(C.pointer, list_tight)
+   end
+
+   C.get_list_start = function()
+      return cmark.cmark_node_get_list_start(C.pointer)
+   end
+
+   C.set_list_start = function(list_start)
+      return cmark.cmark_node_set_list_start(C.pointer, list_start)
+   end
+
+   C.get_fence_info = function()
+      return cmark.cmark_node_get_fence_info(C.pointer)
+   end
+
+   C.set_fence_info = function(fence_info)
+      return cmark.cmark_node_set_fence_info(C.pointer, fence_info)
+   end
+
+   C.render_html = function()
+      return cmark.cmark_render_html(C.pointer)
+   end
+
    return C
 end
 
@@ -85,3 +169,6 @@ function cmark.parse_string(s)
    return cursor
 end
 
+-- testing:
+
+cur = cmark.parse_string("* hi")
