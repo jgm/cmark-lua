1c139fcfbbbfc85b4b1969ad40a9d3ccc05c17c4
John MacFarlane <jgm@berkeley.edu>
2014-12-10 16:16:16 -0800

    Semi-working man writer.

--- man.lua
+++ man.lua
@@ -40,44 +40,26 @@ local cr = Man.cr
 local escape = Man.escape
 local urlencode = Man.urlencode
 
-function Man.tag_open(tag, attrs)
-   if Man.notags > 0 then return end
-   out('<' .. tag)
-   if attrs then
-      for k, v in pairs(attrs) do
-         out(' ' .. k .. '="' .. escape(v) .. '"')
-      end
-   end
-   out('>')
-end
-
-function Man.tag_close(tag)
-   if Man.notags > 0 then return end
-   out('</' .. tag .. '>')
-end
-
-function Man.tag_selfclosing(tag, attrs)
-   if Man.notags > 0 then return end
-   out('<' .. tag)
-   if attrs then
-      for k, v in pairs(attrs) do
-         out(' ' .. k .. '="' .. escape(v) .. '"')
-      end
-   end
-   out(' />')
-end
-
-local tag_open, tag_close, tag_selfclosing = Man.tag_open,
-  Man.tag_close, Man.tag_selfclosing
-
 function Man.begin_document()
+   Man.list_type = {}
+   Man.list_number = {}
+   title = "title"
+   section = "1"
+   footer = "footer"
+   header = "header"
+   date = os.date("%B %d, %Y")
+   out(format('.TH "%s" "%s" "%s" "%s" "%s"',
+              title, section, date, footer, header))
+   cr()
 end
 
 function Man.end_document()
+   cr()
 end
 
 function Man.begin_paragraph(tight)
    if not tight then
+      cr()
       out(".PP")
       cr()
    end
@@ -88,17 +70,19 @@ function Man.end_paragraph(tight)
 end
 
 function Man.begin_block_quote()
-   tag_open('blockquote')
+   cr()
+   out(".RS")
    cr()
 end
 
 function Man.end_block_quote()
    cr()
-   tag_close('blockquote')
+   out(".RE")
    cr()
 end
 
 function Man.begin_header(level)
+   cr()
    if level == 1 then
       out(".SH")
    else
@@ -113,59 +97,46 @@ end
 
 function Man.code_block(s, info)
    cr()
-   attrs = {}
-   if #info > 0 then
-      attrs.class = 'language-' .. gsub(info,' .*$','')
-   end
-   tag_open('pre')
-   tag_open('code', attrs)
+   out(".IP\n.nf\n\\f[C]\n")
    out(escape(s))
-   tag_close('code')
-   tag_close('pre')
+   cr()
+   out("\\f[]\n.fi")
    cr()
 end
 
 function Man.html(s)
-   cr()
-   out(s)
-   cr()
 end
 
 function Man.hrule()
    cr()
-   tag_selfclosing('hr')
+   out(".PP\n  *  *  *  *  *")
    cr()
 end
 
 function Man.begin_list(stype, tight, start)
-   cr()
-   local tag
-   if stype == 'bullet' then tag = 'ul' else tag = 'ol' end
-   local attrs = {}
-   if start > 1 then
-      attrs.start = start
-   end
-   tag_open(tag, attrs)
-   cr()
+   table.insert(Man.list_type, stype)
+   table.insert(Man.list_number, start)
 end
 
 function Man.end_list(stype)
-   cr()
-   local tag
-   if stype == 'bullet' then tag = 'ul' else tag = 'ol' end
-   tag_close(tag)
-   cr()
+   table.remove(Man.list_type)
+   table.remove(Man.list_number)
 end
 
 function Man.begin_list_item(tight)
    cr()
-   tag_open('li')
-   if not tight then cr() end
+   out(".IP ")
+   if Man.list_type[#Man.list_type] == 'bullet' then
+      out("\\[bu] 2")
+   else
+      out(format('"%d." 4', Man.list_number[#Man.list_type]))
+      Man.list_number[#Man.list_number] =
+         Man.list_number[#Man.list_number] + 1
+   end
+   cr()
 end
 
 function Man.end_list_item(tight)
-   if not tight then cr() end
-   tag_close('li')
    cr()
 end
 
@@ -178,68 +149,48 @@ function Man.softbreak()
 end
 
 function Man.linebreak()
-   tag_selfclosing('br')
+   out(".PD 0\n.P\n.PD")
    cr()
 end
 
 function Man.inline_code(s)
-   tag_open('code')
+   out("\\f[C]")
    out(escape(s))
-   tag_close('code')
+   out("\\f[]")
 end
 
 function Man.inline_html(s)
-   out(s)
 end
 
 function Man.begin_emph()
-   tag_open('em')
+   out("\\f[I]")
 end
 
 function Man.end_emph()
-   tag_close('em')
+   out("\\f[]")
 end
 
 function Man.begin_strong()
-   tag_open('strong')
+   out("\\f[B]")
 end
 
 function Man.end_strong()
-   tag_close('strong')
+   out("\\f[]")
 end
 
 function Man.begin_link(url, title)
-   local attrs = {href = url}
-   if #title > 0 then
-      attrs.title = title
-   end
-   tag_open('a', attrs)
 end
 
 function Man.end_link(url, title)
-   tag_close('a')
+   out(format(" (%s)", escape(url)))
 end
 
 function Man.begin_image(url, title)
-   if Man.notags == 0 then
-      out('<img src="')
-      out(urlencode(url))
-      out('"')
-      if #title > 0 then
-         out(' title="')
-         out(escape(title))
-         out('"')
-      end
-      out(' alt="')
-      Man.notags = Man.notags + 1
-   end
+   out("[IMAGE: ")
 end
 
 function Man.end_image(url, title)
-   Man.notags = Man.notags - 1
-   if Man.notags == 0 then
-      out('" />')
-   end
+   out("]")
 end
 
 return Man
