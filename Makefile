2ed4e5fd92505bb5c1e65d9d09aaba23bf0f8153
John MacFarlane <jgm@berkeley.edu>
2015-12-15 14:27:37 -0800

    Got it working with new cmark sources.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS = -fPIC -O3 -I$(EXT) -I.
+CFLAGS = -fPIC -O3 -I$(CBITS) -I.
 LIBFLAG = -shared
 LUA_LIBDIR = /usr/local/lib/lua/5.2
 LUA_BINDIR = /usr/local/bin
@@ -12,16 +12,16 @@ INST_LUADIR = $(INST_PREFIX)/share/lua/5.2
 INST_CONFDIR = $(INST_PREFIX)/etc
 SWIG ?= swig
 CMARK_DIR ?= ../cmark
-OBJS = $(subst .c,.o,$(C_SOURCES))
-C_SOURCES=$(wildcard $(EXT)/*.*)
-EXT = cbits
+OBJS = $(subst .c,.o,$(wildcard $(CBITS)/*.c))
+C_SOURCES=$(wildcard $(CBITS)/*.*)
+CBITS = cbits
 
 .PHONY: clean, distclean, test, install, all
 
 all: cmark.so cmark-lua
 
 cmark.so: cmark_wrap.o $(OBJS)
-	$(CC) $(LIBFLAG) -o $@ -L$(EXT) -L$(LUA_LIBDIR) -I$(EXT) -I. -llua $^
+	$(CC) $(LIBFLAG) -o $@ -L$(CBITS) -L$(LUA_LIBDIR) -I$(CBITS) -I. -llua $^
 
 cmark.o: cmark_wrap.c
 	$(CC) -c $(CFLAGS) $< -o $@
@@ -34,20 +34,20 @@ $(LUADIR)/liblua.a: $(wildcard $(LUADIR)/*.h) $(wildcard $(LUADIR)/*.c) $(LUADIR
 	# note: LUA_USE_LINUX is recommended for linux, osx, freebsd
 
 cmark-lua: main.o cmark_wrap.o $(OBJS) $(LUADIR)/liblua.a luautf8/lutf8lib.o
-	$(CC) -o $@ -L$(EXT) -I$(EXT) -I. $^
+	$(CC) -o $@ -L$(CBITS) -I$(CBITS) -I. $^
 
 update-c-sources: $(C_SOURCES)
 
-$(EXT)/config.h: $(CMARK_DIR)/build/src/config.h
+$(CBITS)/config.h: $(CMARK_DIR)/build/src/config.h
 	cp $< $@
 
-$(EXT)/cmark_export.h: $(CMARK_DIR)/build/src/cmark_export.h
+$(CBITS)/cmark_export.h: $(CMARK_DIR)/build/src/cmark_export.h
 	cp $< $@
 
-$(EXT)/cmark_version.h: $(CMARK_DIR)/build/src/cmark_version.h
+$(CBITS)/cmark_version.h: $(CMARK_DIR)/build/src/cmark_version.h
 	cp $< $@
 
-$(EXT)/%: $(CMARK_DIR)/src/%
+$(CBITS)/%: $(CMARK_DIR)/src/%
 	cp $< $@
 
 install: cmark.so
@@ -55,11 +55,11 @@ install: cmark.so
 	install $< $(INST_LIBDIR)/
 	# cp lua file to $(INST_LUADIR)
 
-cmark_wrap.c: cmark.i
-	$(SWIG) -o $@ -includeall -lua -I$(EXT) -Idummy $<
+cmark_wrap.c: cmark.i $(CBITS)/cmark.h
+	$(SWIG) -o $@ -includeall -lua -I$(CBITS) -Idummy $<
 
 test:
 	python $(CMARK)/test/spec_tests.py --spec $(CMARK)/spec.txt --prog ./wrap.lua
 
 clean:
-	rm -rf cmark.so *.o $(EXT)/*.o $(LUADIR)/*.[oa] cmark-lua luautf8/*.o
+	rm -rf cmark.so *.o $(CBITS)/*.o $(LUADIR)/*.[oa] cmark-lua luautf8/*.o
--- main.c
+++ main.c
@@ -2,157 +2,201 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include "config.h"
+#include "cmark.h"
+#include "bench.h"
+
+#if defined(_WIN32) && !defined(__CYGWIN__)
+#include <io.h>
+#include <fcntl.h>
+#endif
 
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 
-#include "config.h"
-#include "cmark.h"
-#include "debug.h"
-#include "bench.h"
-
 extern int luaopen_cmark(lua_State *L);
 extern int luaopen_utf8(lua_State *L);
 extern void push_cmark_node(lua_State *L, cmark_node *node);
 
-void print_usage()
-{
-	printf("Usage:   cmark [FILE*]\n");
-	printf("Options: --help, -h    Print usage information\n");
-	printf("         --ast         Print AST instead of HTML\n");
-	printf("         --version     Print version\n");
+typedef enum {
+  FORMAT_NONE,
+  FORMAT_HTML,
+  FORMAT_XML,
+  FORMAT_MAN,
+  FORMAT_COMMONMARK,
+  FORMAT_LATEX
+} writer_format;
+
+void print_usage() {
+  printf("Usage:   cmark [FILE*]\n");
+  printf("Options:\n");
+  printf("  --to, -t FORMAT  Specify output format (html, xml, man, "
+         "commonmark, latex)\n");
+  printf("  --width WIDTH    Specify wrap width (default 0 = nowrap)\n");
+  printf("  --sourcepos      Include source position attribute\n");
+  printf("  --hardbreaks     Treat newlines as hard line breaks\n");
+  printf("  --safe           Suppress raw HTML and dangerous URLs\n");
+  printf("  --smart          Use smart punctuation\n");
+  printf("  --normalize      Consolidate adjacent text nodes\n");
+  printf("  --help, -h       Print usage information\n");
+  printf("  --version        Print version\n");
 }
 
-static void print_document(cmark_node *document, bool ast)
-{
-	char *result;
-	if (ast) {
-		result = cmark_render_ast(document);
-	} else {
+static void print_document(cmark_node *document, writer_format writer,
+                           int options, int width) {
+  char *result;
 
-		result = cmark_render_html(document);
-	}
-	printf("%s", result);
-	free(result);
+  switch (writer) {
+  case FORMAT_HTML:
+    result = cmark_render_html(document, options);
+    break;
+  case FORMAT_XML:
+    result = cmark_render_xml(document, options);
+    break;
+  case FORMAT_MAN:
+    result = cmark_render_man(document, options, width);
+    break;
+  case FORMAT_COMMONMARK:
+    result = cmark_render_commonmark(document, options, width);
+    break;
+  case FORMAT_LATEX:
+    result = cmark_render_latex(document, options, width);
+    break;
+  default:
+    fprintf(stderr, "Unknown format %d\n", writer);
+    exit(1);
+  }
+  printf("%s", result);
+  free(result);
 }
 
-int main(int argc, char *argv[])
-{
-	int i, numfps = 0;
-	bool ast = false;
-	int *files;
-	char buffer[4096];
-	cmark_parser *parser;
-	size_t bytes;
-	cmark_node *document;
-	int luafile = 0;
-	int status = 0;
+int main(int argc, char *argv[]) {
+  int i, numfps = 0;
+  int *files;
+  char buffer[4096];
+  cmark_parser *parser;
+  size_t bytes;
+  cmark_node *document;
+  int width = 0;
+  char *unparsed;
+  writer_format writer = FORMAT_HTML;
+  int options = CMARK_OPT_DEFAULT;
 
-	parser = cmark_parser_new();
-	files = (int *)malloc(argc * sizeof(*files));
+#if defined(_WIN32) && !defined(__CYGWIN__)
+  _setmode(_fileno(stdout), _O_BINARY);
+#endif
 
-	for (i = 1; i < argc; i++) {
-		if (strcmp(argv[i], "--version") == 0) {
-			printf("cmark %s", CMARK_VERSION);
-			printf(" - CommonMark converter (c) 2014 John MacFarlane\n");
-			exit(0);
-		} else if ((strcmp(argv[i], "--help") == 0) ||
-			   (strcmp(argv[i], "-h") == 0)) {
-			print_usage();
-			exit(0);
-		} else if (strcmp(argv[i], "--ast") == 0) {
-			ast = true;
-		} else if (strcmp(argv[i], "--lua") == 0) {
-			if (i + 1 < argc) {
-				luafile = i + 1;
-				i++;
-			} else {
-				fprintf(stderr, "No --lua file specified\n");
-				exit(1);
-			}
-		} else if (*argv[i] == '-') {
-			print_usage();
-			exit(1);
-		} else { // treat as file argument
-			files[numfps++] = i;
-		}
-	}
+  files = (int *)malloc(argc * sizeof(*files));
 
-	for (i = 0; i < numfps; i++) {
-		FILE *fp = fopen(argv[files[i]], "r");
-		if (fp == NULL) {
-			fprintf(stderr, "Error opening file %s: %s\n",
-				argv[files[i]], strerror(errno));
-			exit(1);
-		}
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--version") == 0) {
+      printf("cmark %s", CMARK_VERSION_STRING);
+      printf(" - CommonMark converter\n(C) 2014, 2015 John MacFarlane\n");
+      exit(0);
+    } else if (strcmp(argv[i], "--sourcepos") == 0) {
+      options |= CMARK_OPT_SOURCEPOS;
+    } else if (strcmp(argv[i], "--hardbreaks") == 0) {
+      options |= CMARK_OPT_HARDBREAKS;
+    } else if (strcmp(argv[i], "--smart") == 0) {
+      options |= CMARK_OPT_SMART;
+    } else if (strcmp(argv[i], "--safe") == 0) {
+      options |= CMARK_OPT_SAFE;
+    } else if (strcmp(argv[i], "--normalize") == 0) {
+      options |= CMARK_OPT_NORMALIZE;
+    } else if (strcmp(argv[i], "--validate-utf8") == 0) {
+      options |= CMARK_OPT_VALIDATE_UTF8;
+    } else if ((strcmp(argv[i], "--help") == 0) ||
+               (strcmp(argv[i], "-h") == 0)) {
+      print_usage();
+      exit(0);
+    } else if (strcmp(argv[i], "--width") == 0) {
+      i += 1;
+      if (i < argc) {
+        width = (int)strtol(argv[i], &unparsed, 10);
+        if (unparsed && strlen(unparsed) > 0) {
+          fprintf(stderr, "failed parsing width '%s' at '%s'\n", argv[i],
+                  unparsed);
+          exit(1);
+        }
+      } else {
+        fprintf(stderr, "--width requires an argument\n");
+        exit(1);
+      }
+    } else if ((strcmp(argv[i], "-t") == 0) || (strcmp(argv[i], "--to") == 0)) {
+      i += 1;
+      if (i < argc) {
+        if (strcmp(argv[i], "man") == 0) {
+          writer = FORMAT_MAN;
+        } else if (strcmp(argv[i], "html") == 0) {
+          writer = FORMAT_HTML;
+        } else if (strcmp(argv[i], "xml") == 0) {
+          writer = FORMAT_XML;
+        } else if (strcmp(argv[i], "commonmark") == 0) {
+          writer = FORMAT_COMMONMARK;
+        } else if (strcmp(argv[i], "latex") == 0) {
+          writer = FORMAT_LATEX;
+        } else {
+          fprintf(stderr, "Unknown format %s\n", argv[i]);
+          exit(1);
+        }
+      } else {
+        fprintf(stderr, "No argument provided for %s\n", argv[i - 1]);
+        exit(1);
+      }
+    } else if (*argv[i] == '-') {
+      print_usage();
+      exit(1);
+    } else { // treat as file argument
+      files[numfps++] = i;
+    }
+  }
 
-		start_timer();
-		while ((bytes = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
-			cmark_parser_feed(parser, buffer, bytes);
-			if (bytes < sizeof(buffer)) {
-				break;
-			}
-		}
-		end_timer("processing lines");
+  parser = cmark_parser_new(options);
+  for (i = 0; i < numfps; i++) {
+    FILE *fp = fopen(argv[files[i]], "r");
+    if (fp == NULL) {
+      fprintf(stderr, "Error opening file %s: %s\n", argv[files[i]],
+              strerror(errno));
+      exit(1);
+    }
 
-		fclose(fp);
-	}
+    start_timer();
+    while ((bytes = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
+      cmark_parser_feed(parser, buffer, bytes);
+      if (bytes < sizeof(buffer)) {
+        break;
+      }
+    }
+    end_timer("processing lines");
 
-	if (numfps == 0) {
-		/*
-		document = cmark_parse_file(stdin);
-		print_document(document, ast);
-		exit(0);
-		*/
+    fclose(fp);
+  }
 
-		while ((bytes = fread(buffer, 1, sizeof(buffer), stdin)) > 0) {
-			cmark_parser_feed(parser, buffer, bytes);
-			if (bytes < sizeof(buffer)) {
-				break;
-			}
-		}
-	}
+  if (numfps == 0) {
 
-	start_timer();
-	document = cmark_parser_finish(parser);
-	end_timer("finishing document");
-	cmark_parser_free(parser);
+    while ((bytes = fread(buffer, 1, sizeof(buffer), stdin)) > 0) {
+      cmark_parser_feed(parser, buffer, bytes);
+      if (bytes < sizeof(buffer)) {
+        break;
+      }
+    }
+  }
 
-	if (luafile) {
+  start_timer();
+  document = cmark_parser_finish(parser);
+  end_timer("finishing document");
+  cmark_parser_free(parser);
 
-		lua_State *L = luaL_newstate();
-		luaL_openlibs(L);
-		luaL_requiref(L, "utf8", luaopen_utf8, 1);
-		luaL_requiref(L, "cmark", luaopen_cmark, 1);
-		status = luaL_loadfile(L, argv[luafile]) ||
-			lua_pcall(L, 0, 1, 0);
-		if (status != 0) {
-			fprintf(stderr, "%s\n", lua_tostring(L, -1));
-			return 3;
-		} else {
-			lua_getfield(L, -1, "render");
-			push_cmark_node(L, document);
-			if (lua_pcall(L, 1, 1, 0) != 0) {
-				fprintf(stderr, "Error running render: %s\n", lua_tostring(L, -1));
-				return 5;
-			}
-			printf("%s", lua_tostring(L, -1));
-		}
-		lua_close(L);
+  start_timer();
+  print_document(document, writer, options, width);
+  end_timer("print_document");
 
-	} else {
+  start_timer();
+  cmark_node_free(document);
+  end_timer("free_blocks");
 
-		start_timer();
-		print_document(document, ast);
-		end_timer("print_document");
+  free(files);
 
-	}
-
-	start_timer();
-	cmark_node_free(document);
-	end_timer("free_blocks");
-	free(files);
-
-	return 0;
+  return 0;
 }
