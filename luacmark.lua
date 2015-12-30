2d0338cc02b4f5a3afd673ec68024146838ca7b0
John MacFarlane <jgm@berkeley.edu>
2015-12-30 12:41:02 -0800

    Add luacmark.format, that does all the plumbing for you.
    Removed `-d/--data` option for now.

--- luacmark.lua
+++ luacmark.lua
@@ -1,3 +1,5 @@
+-- local inspect = require('inspect').inspect -- TODO debugging
+
 local cmark = require("cmark")
 local yaml = require("yaml")
 local Lust = require("lust")
@@ -13,6 +15,47 @@ luacmark.writers = {
   latex = cmark.render_latex,
   commonmark = cmark.render_commonmark
 }
+
+luacmark.defaults = {
+  smart = false,
+  hardbreaks = false,
+  sourcepos = false,
+  safe = false,
+  columns = 0,
+}
+
+local toOptions = function(opts)
+  if type(opts) == 'table' then
+    return (cmark.OPT_VALIDATE_UTF8 + cmark.OPT_NORMALIZE +
+      (opts.smart and cmark.OPT_SMART or 0) +
+      (opts.safe and cmark.OPT_SAFE or 0) +
+      (opts.hardbreaks and cmark.OPT_HARDBREAKS or 0) +
+      (opts.sourcepos and cmark.OPT_SOURCEPOS or 0)
+      )
+  else
+     return opts
+  end
+end
+
+-- walk nodes of table, applying a callback to each
+local function walk_table(table, callback, inplace)
+  assert(type(table) == 'table')
+  local new = {}
+  local res
+  for k, v in pairs(table) do
+    if type(v) == 'table' then
+      res = walk_table(v, callback, inplace)
+    else
+      res = callback(v)
+    end
+    if not inplace then
+      new[k] = res
+    end
+  end
+  if not inplace then
+    return new
+  end
+end
 
 
 local defaultEnv = _ENV
@@ -38,9 +81,14 @@ end
 -- working directory, then in `templates`, then in
 -- `$HOME/.lunamark/templates`, then in the Windows
 -- `APPDATA` directory.
-function luacmark.find_template(name)
+function luacmark.find_template(name, to)
+  if not name then
+    return false, "Missing template name"
+  end
   local base, ext = name:match("([^%.]*)(.*)")
-  if (not ext or ext == "") and format then ext = "." .. format end
+  if (not ext or ext == "") and to then
+    ext = "." .. to
+  end
   local fname = base .. ext
   local file = io.open(fname, "r")
   if not file then
@@ -86,25 +134,18 @@ local render_metadata = function(node, writer, options, columns)
   end
 end
 
--- Iterate over the metadata, converting to target format.
--- Returns a new table with rendered metadata.
-function luacmark.render_metadata(table, writer, options, columns)
-  local rendered = {}
-  for i, v in pairs(table) do
-    if type(v) == 'table' then
-        rendered[i] = convert_metadata(v, writer, options, columns)
-    else
-        local vs = tostring(v)
-        rendered[i] = render_metadata(cmark.parse_string(vs, options),
-                                      writer, options, columns)
-    end
-  end
-  return rendered
+-- Iterate over the metadata, converting to cmark nodes.
+-- Returns a new table.
+local convert_metadata = function(table, options)
+  return walk_table(table,
+                    function(s)
+                      return cmark.parse_string(tostring(s), options)
+                    end, false)
 end
 
 -- Parses document with optional front YAML metadata; returns document,
 -- metadata.
-function luacmark.parse_document_with_metadata(inp, options)
+local parse_document_with_metadata = function(inp, options)
   local metadata = {}
   if string.find(inp, '^---[\r\n]') then
     local _, endlast = string.find(inp, '[\r\n]...[ \t]*[\r\n][\r\n\t ]*', 3)
@@ -112,10 +153,12 @@ function luacmark.parse_document_with_metadata(inp, options)
       local _, endlast = string.find(inp, '[\r\n]---[ \t]*[\r\n][\r\n\t ]*', 3)
     end
     if endlast then
-      metadata = yaml.load(string.sub(inp, 1, endlast))
+      local yaml_meta = yaml.load(string.sub(inp, 1, endlast))
+      metadata = convert_metadata(yaml_meta, options)
       if type(metadata) ~= 'table' then
         metadata = {}
-      else
+      end
+      if yaml_meta then
         -- We insert blank lines where the header was, so sourcepos is accurate:
         inp = string.gsub(string.sub(inp, 1, endlast - 1), '[^\n\r]+', '') ..
            string.sub(inp, endlast)
@@ -124,6 +167,41 @@ function luacmark.parse_document_with_metadata(inp, options)
   end
   doc = cmark.parse_document(inp, string.len(inp), options)
   return doc, metadata
+end
+
+-- 'inp' is the string input source.
+-- 'options' is a table with fields 'smart', 'hardbreaks',
+-- 'safe', 'sourcepos' (all boolean) and 'columns' (number,
+-- 0 for no wrapping).
+-- 'callback' is a filter or nil.
+-- 'template' is a Lust template to be filled with the document
+-- body and rendered metadata, or nil.
+-- TODO handle errors
+function luacmark.convert(inp, to, options, callback, template)
+  local opts = toOptions(options)
+  local columns = options.columns or 0
+  local doc, meta = parse_document_with_metadata(inp, opts)
+  if callback then
+    -- apply callback to nodes of metadata
+    walk_table(meta, function(node) callback(node, to) end, true)
+    callback(doc, to)
+  end
+  local writer = luacmark.writers[to]
+  local body = writer(doc, opts, columns)
+  local data = walk_table(meta,
+                          function(node)
+                            local res = render_metadata(node, writer, opts, columns)
+                            return res
+                          end, false)
+  -- free memory allocated by libcmark
+  cmark.node_free(doc)
+  walk_table(meta, cmark.node_free, true)
+  if template then
+    data.body = body
+    return Lust(template):gen(data)
+  else
+    return body
+  end
 end
 
 return luacmark
