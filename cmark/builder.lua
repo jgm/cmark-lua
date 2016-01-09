7e1029aae7d96d0c472804b62d811879c2ca1e06
John MacFarlane <jgm@berkeley.edu>
2016-01-09 13:50:00 -0800

    Progress on builder module.

--- cmark/builder.lua
+++ cmark/builder.lua
@@ -1,3 +1,36 @@
+local c = require('cmark')
+
 local builder = {}
+
+builder.add_children = function(node, v)
+  if type(v) == 'table' then
+    for _,x in ipairs(x) do
+      c.node_append_child(node, x)
+    end
+  elseif type(v) == 'userdata' then
+    c.node_append_child(node, v)
+  else
+    c.node_append_child(node, builder.text(tostring(v)))
+  end
+end
+
+-- builder.text("hello")
+builder.text = function(s)
+  local node = c.node_new(c.NODE_TEXT)
+  c.node_set_literal(node, s)
+  return node
+end
+
+-- builder.link({url = "/url", title = "my title",
+--   link_text = "my text"})
+-- link_text may be a string, a node, or a list
+-- of nodes.
+builder.link = function(data)
+  local node = c.node_new(c.NODE_LINK)
+  c.node_set_url(node, data.url)
+  c.node_set_title(node, data.title)
+  builder.add_children(node, data.link_text)
+  return node
+end
 
 return builder
--- test.t
+++ test.t
@@ -5,6 +5,7 @@ package.path = "./?.lua;" .. package.path
 package.cpath = "./?.so;" .. package.cpath
 
 local cmark = require 'cmark'
+local builder = require 'cmark.builder'
 local tests = require 'spec-tests'
 
 for _,test in ipairs(tests) do
@@ -14,5 +15,14 @@ for _,test in ipairs(tests) do
            " (lines " .. tostring(test.start_line) .. " - " ..
            tostring(test.end_line) .. ")")
 end
+
+local tohtml = function(node)
+  return cmark.render_html(node, cmark.OPT_DEFAULT)
+end
+
+is(tohtml(builder.text("hello")), "hello", "builder.text")
+ is(tohtml(builder.link({url = "url", link_text = "hello"})),
+    '<a href="url">hello</a>', "builder.link")
+
 
 done_testing()
