71ba1c35c347dd7fc6d47ee2d5e8ce4d5121e414
John MacFarlane <jgm@berkeley.edu>
2016-01-12 08:22:19 -0800

    Improved builders for lists.
    Now we have separate builders for bullet_list and ordered_list.

--- cmark/builder.lua
+++ cmark/builder.lua
@@ -6,14 +6,17 @@ end
 
 local builder = {}
 
-local is_block = function(node)
+-- returns 'inline', 'block', 'item', or 'unknown'
+local node_get_class = function(node)
   local nt = node_get_type(node)
-  return (nt >= NODE_FIRST_BLOCK and node <= NODE_LAST_BLOCK)
-end
-
-local is_inline = function(node)
-  local nt = node_get_type(node)
-  return (nt >= NODE_FIRST_INLINE and node <= NODE_LAST_INLINE)
+  if nt == NODE_ITEM then
+    return 'item'
+  elseif (nt >= NODE_FIRST_BLOCK and nt <= NODE_LAST_BLOCK) then
+    return 'block'
+  elseif (nt >= NODE_FIRST_INLINE and nt <= NODE_LAST_INLINE) then
+    return 'inline'
+  end
+  return 'unknown'
 end
 
 -- builder.add_children(node, {node1, node2})
@@ -24,18 +27,53 @@ end
 -- adds a text node with 'hello' as child of node.
 -- builder.add_children(node, node1)
 -- adds node1 as a child of node.
-builder.add_children = function(node, v)
+-- contains is a table with fields items, blocks, inlines
+-- that tells you what kind of children the table can contain
+-- returns true or nil, msg.
+builder.add_children = function(node, v, contains)
+  if type(v) == 'nil' then
+    return true -- just skip a nil
+  end
   if type(v) == 'table' then
     for _,x in ipairs(v) do
-      builder.add_children(node, x)
+      builder.add_children(node, x, contains)
     end
-  elseif type(v) == 'userdata' then
-    node_append_child(node, v)
-  elseif v ~= nil then
-    local textnode = node_new(NODE_TEXT)
-    node_set_literal(textnode, tostring(v))
-    node_append_child(node, textnode)
+    return true
   end
+  local child
+  -- if v is not a node, make a text node:
+  if type(v) == 'userdata' then
+    child = v
+  else
+    child = node_new(NODE_TEXT)
+    node_set_literal(child, tostring(v))
+  end
+  local child_class = node_get_class(child)
+  if child_class == 'item' and contains.items then
+    node_append_child(node, child)
+  elseif child_class == 'block' and contains.blocks then
+    node_append_child(node, child)
+  elseif child_class == 'inline' and contains.inlines then
+    node_append_child(node, child)
+  elseif child_class == 'block' and contains.items then
+    local item = node_new(NODE_ITEM)
+    node_append_child(item, child)
+    node_append_child(node, item)
+  elseif child_class == 'inline' and contains.blocks then
+    local para = node_new(NODE_PARAGRAPH)
+    node_append_child(para, child)
+    node_append_child(node, para)
+  elseif child_class == 'inline' and contains.items then
+    local para = node_new(NODE_PARAGRAPH)
+    local item = node_new(NODE_ITEM)
+    node_append_child(para, child)
+    node_append_child(item, para)
+    node_append_child(node, item)
+  else
+    return nil, 'Tried to add a node with class ' .. child_class ..
+                'to a node with class ' .. node_get_class(node)
+  end
+  return true
 end
 
 -- return children as a table
@@ -61,10 +99,6 @@ builder.node = function(node_type, contains, fields)
     if fields and type(contents) == 'table' then
       for field,func in pairs(fields) do
         if contents[field] then
-          if type(contents[field]) == 'boolean' then
-            -- C functions take int not bool
-            contents[field] = 1
-          end
           func(node, contents[field])
         end
       end
@@ -80,49 +114,28 @@ builder.node = function(node_type, contains, fields)
   end
 end
 
-local toitems = function(items)
-  local newitems = {}
-  newitems.tight = items.tight
-  newitems.start = items.start
-  newitems.delim = items.delim
-  for _,item in ipairs(items) do
-    if type(item) == 'userdata' then
-      local itemtype = c.node_get_type(item)
-      if itemtype == c.NODE_ITEM then
-        table.insert(newitems, item)
-      elseif itemtype >= c.NODE_FIRST_BLOCK and itemtype <= c.NODE_LAST_BLOCK then
-        table.insert(newitems, builder.item(item))
-      else  -- inline
-        table.insert(newitems, builder.item(builder.paragraph(item)))
-      end
-    elseif type(item) == 'table' then
-        table.insert(newitems, insert(item))
-    else
-        table.insert(newitems, builder.item(builder.paragraph(tostring(item))))
-    end
-  end
-  return newitems
+local function set_tight(node, tight)
+  local t_int = tight and 1 or 0
+  return node_set_list_tight(node, t_int)
 end
 
 builder.document = builder.node(NODE_DOCUMENT, {blocks = true})
 builder.block_quote = builder.node(NODE_BLOCK_QUOTE, {blocks = true})
-builder.list = builder.node(NODE_LIST, {items = true},
-                 {list_type = node_set_list_type,
+builder.ordered_list = builder.node(NODE_LIST, {items = true},
+                 {list_type = function(n,_)
+                     return node_set_list_type(n, c.ORDERED_LIST)
+                  end,
                   delim = node_set_list_delim,
                   start = node_set_list_start,
-                  tight = node_set_list_tight})
+                  tight = set_tight,
+                 })
+builder.bullet_list = builder.node(NODE_LIST, {items = true},
+                 {list_type = function(n,_)
+                     return node_set_list_type(n, c.BULLET_LIST)
+                  end,
+                  tight = set_tight,
+                 })
 builder.item = builder.node(NODE_ITEM, {blocks = true})
-builder.bullet_list = function(contents)
-                        local newcontents = toitems(contents)
-                        newcontents.list_type = c.BULLET_LIST
-                        return builder.list(newcontents)
-                      end
-builder.ordered_list = function(contents)
-                        local newcontents = toitems(contents)
-                        newcontents.list_type = c.ORDERED_LIST
-                        return builder.list(newcontents)
-                      end
-
 builder.paragraph = builder.node(NODE_PARAGRAPH, {inlines = true})
 builder.text = builder.node(NODE_TEXT, {literal = true})
 builder.emph = builder.node(NODE_EMPH, {inlines = true})
