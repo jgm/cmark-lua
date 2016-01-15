98c9e2fbe0a22fef2fd5f6799b88422a1e6dfebf
John MacFarlane <jgm@berkeley.edu>
2016-01-14 23:51:41 -0800

    Used qualified names in library.

--- cmark/builder.lua
+++ cmark/builder.lua
@@ -1,19 +1,15 @@
 local c = require('cmark')
 
-for k,v in pairs(c) do
-  _ENV[k] = v
-end
-
 local builder = {}
 
 -- returns 'inline', 'block', 'item', or 'unknown'
 local node_get_class = function(node)
-  local nt = node_get_type(node)
-  if nt == NODE_ITEM then
+  local nt = c.node_get_type(node)
+  if nt == c.NODE_ITEM then
     return 'item'
-  elseif (nt >= NODE_FIRST_BLOCK and nt <= NODE_LAST_BLOCK) then
+  elseif (nt >= c.NODE_FIRST_BLOCK and nt <= c.NODE_LAST_BLOCK) then
     return 'block'
-  elseif (nt >= NODE_FIRST_INLINE and nt <= NODE_LAST_INLINE) then
+  elseif (nt >= c.NODE_FIRST_INLINE and nt <= c.NODE_LAST_INLINE) then
     return 'inline'
   end
   return 'unknown'
@@ -52,34 +48,34 @@ add_children = function(node, v, contains)
   if type(v) == 'userdata' then
     child = v
   elseif contains.literal then
-    node_set_literal(node, tostring(v))
+    c.node_set_literal(node, tostring(v))
     return true
   else
     -- if v is not a node, make a text node:
-    child = node_new(NODE_TEXT)
-    node_set_literal(child, tostring(v))
+    child = c.node_new(c.NODE_TEXT)
+    c.node_set_literal(child, tostring(v))
   end
   local child_class = node_get_class(child)
   if child_class == 'item' and contains.items then
-    node_append_child(node, child)
+    c.node_append_child(node, child)
   elseif child_class == 'block' and contains.blocks then
-    node_append_child(node, child)
+    c.node_append_child(node, child)
   elseif child_class == 'inline' and contains.inlines then
-    node_append_child(node, child)
+    c.node_append_child(node, child)
   elseif child_class == 'block' and contains.items then
-    local item = node_new(NODE_ITEM)
-    node_append_child(item, child)
-    node_append_child(node, item)
+    local item = c.node_new(c.NODE_ITEM)
+    c.node_append_child(item, child)
+    c.node_append_child(node, item)
   elseif child_class == 'inline' and contains.blocks then
-    local para = node_new(NODE_PARAGRAPH)
-    node_append_child(para, child)
-    node_append_child(node, para)
+    local para = c.node_new(c.NODE_PARAGRAPH)
+    c.node_append_child(para, child)
+    c.node_append_child(node, para)
   elseif child_class == 'inline' and contains.items then
-    local para = node_new(NODE_PARAGRAPH)
-    local item = node_new(NODE_ITEM)
-    node_append_child(para, child)
-    node_append_child(item, para)
-    node_append_child(node, item)
+    local para = c.node_new(c.NODE_PARAGRAPH)
+    local item = c.node_new(c.NODE_ITEM)
+    c.node_append_child(para, child)
+    c.node_append_child(item, para)
+    c.node_append_child(node, item)
   else
     return nil, 'Tried to add a node with class ' .. child_class ..
                 'to a node with class ' .. node_get_class(node)
@@ -102,7 +98,7 @@ end
 -- 'items'
 local node = function(node_type, contains, fields)
   return function(contents)
-    local node = node_new(node_type)
+    local node = c.node_new(node_type)
     if not node then
       return nil, 'Could not create node of type ' .. tostring(node_type)
     end
@@ -131,7 +127,7 @@ end
 
 local function set_tight(n, tight)
   local t_int = tight and 1 or 0
-  return node_set_list_tight(n, t_int)
+  return c.node_set_list_tight(n, t_int)
 end
 
 local function set_delim(n, delim)
@@ -145,70 +141,70 @@ local function set_delim(n, delim)
   else
     return nil, 'Unknown delimiter ' .. delim
   end
-  return node_set_list_delim(n, delimt)
+  return c.node_set_list_delim(n, delimt)
 end
 
-builder.document = node(NODE_DOCUMENT, {blocks = true})
+builder.document = node(c.NODE_DOCUMENT, {blocks = true})
 
-builder.block_quote = node(NODE_BLOCK_QUOTE, {blocks = true})
+builder.block_quote = node(c.NODE_BLOCK_QUOTE, {blocks = true})
 
 builder.ordered_list = function(contents)
-  local n = node(NODE_LIST, {items = true},
+  local n = node(c.NODE_LIST, {items = true},
                  {delim = set_delim,
-                  start = node_set_list_start,
+                  start = c.node_set_list_start,
                   tight = set_tight,
                  })(contents)
-  node_set_list_type(n, ORDERED_LIST)
+  c.node_set_list_type(n, c.ORDERED_LIST)
   return n
 end
 
 builder.bullet_list = function(contents)
-  local n = node(NODE_LIST, {items = true},
+  local n = node(c.NODE_LIST, {items = true},
                  {tight = set_tight,
                  })(contents)
-  node_set_list_type(n, BULLET_LIST)
+  c.node_set_list_type(n, c.BULLET_LIST)
   return n
 end
 
-builder.item = node(NODE_ITEM, {blocks = true})
+builder.item = node(c.NODE_ITEM, {blocks = true})
 
-builder.code_block = node(NODE_CODE_BLOCK, {literal = true},
-   { info = node_set_fence_info })
+builder.code_block = node(c.NODE_CODE_BLOCK, {literal = true},
+   { info = c.node_set_fence_info })
 
-builder.html_block = node(NODE_HTML_BLOCK, {literal = true})
+builder.html_block = node(c.NODE_HTML_BLOCK, {literal = true})
 
-builder.custom_block = node(NODE_CUSTOM_BLOCK,
+builder.custom_block = node(c.NODE_CUSTOM_BLOCK,
    {inlines = true, blocks = true, items = true},
-   { on_enter = node_set_on_enter, on_exit = node_set_on_exit })
+   { on_enter = c.node_set_on_enter, on_exit = c.node_set_on_exit })
 
-builder.thematic_break = node(NODE_THEMATIC_BREAK)
+builder.thematic_break = node(c.NODE_THEMATIC_BREAK)
 
-builder.heading = node(NODE_HEADING, {inlines = true},
-  { level = node_set_heading_level })
+builder.heading = node(c.NODE_HEADING, {inlines = true},
+  { level = c.node_set_heading_level })
 
-builder.paragraph = node(NODE_PARAGRAPH, {inlines = true})
+builder.paragraph = node(c.NODE_PARAGRAPH, {inlines = true})
 
-builder.text = node(NODE_TEXT, {literal = true})
+builder.text = node(c.NODE_TEXT, {literal = true})
 
-builder.emph = node(NODE_EMPH, {inlines = true})
+builder.emph = node(c.NODE_EMPH, {inlines = true})
 
-builder.strong = node(NODE_STRONG, {inlines = true})
+builder.strong = node(c.NODE_STRONG, {inlines = true})
 
-builder.link = node(NODE_LINK, {inlines = true},
-                 {title = node_set_title, url = node_set_url})
+builder.link = node(c.NODE_LINK, {inlines = true},
+                 {title = c.node_set_title, url = c.node_set_url})
 
-builder.image = node(NODE_IMAGE, {inlines = true},
-                 {title = node_set_title, url = node_set_url})
+builder.image = node(c.NODE_IMAGE, {inlines = true},
+                 {title = c.node_set_title, url = c.node_set_url})
 
-builder.linebreak = node(NODE_LINEBREAK)
+builder.linebreak = node(c.NODE_LINEBREAK)
 
-builder.softbreak = node(NODE_SOFTBREAK)
+builder.softbreak = node(c.NODE_SOFTBREAK)
 
-builder.code = node(NODE_CODE, {literal = true})
+builder.code = node(c.NODE_CODE, {literal = true})
 
-builder.html_inline = node(NODE_HTML_INLINE, {literal = true})
+builder.html_inline = node(c.NODE_HTML_INLINE, {literal = true})
 
-builder.custom_inline = node(NODE_CUSTOM_INLINE, {inlines = true},
-   { on_enter = node_set_on_enter, on_exit = node_set_on_exit })
+builder.custom_inline = node(c.NODE_CUSTOM_INLINE, {inlines = true},
+   { on_enter = c.node_set_on_enter, on_exit = c.node_set_on_exit })
 
 return builder
